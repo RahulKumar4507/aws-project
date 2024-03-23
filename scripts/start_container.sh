3f367a0d7851ba665ec09b35bff17b528ebd46c5
Rahul Kumar <140480788+RahulKumar4507@users.noreply.github.com>
2024-03-23 22:25:25 +0530

    install&start

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+# install the Docker
+sudo apt install docker -y
+
 # Pull the Docker image from Docker Hub
-echo
+docker pull rahulkumar25/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p5000:5000 rahulkumar25/simple-python-flask-app
